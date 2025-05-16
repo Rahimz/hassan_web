2855ce19e02c33d8bf964b8e45a8ce74f5533456
rahim <rahim.aghareb@gmail.com>
2025-05-16 11:38:51 +0330

    update admin of image

--- multimedias/admin.py
+++ multimedias/admin.py
@@ -5,8 +5,15 @@ from .models import Image, Gallery
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'gallery', 'active', 'short_uuid']
-
+    list_display = ['id', 'title', 'gallery', 'active', 'short_uuid', 'file_size_mb']
+    
+    def file_size_mb(self, obj):
+        if obj.file:
+            size_bytes = obj.file.size
+            size_mb = size_bytes / (1024 * 1024)  # Convert bytes to MB
+            return f"{size_mb:.2f} MB"  # Format to 2 decimal places
+        return "0 MB"
+    file_size_mb.short_description = 'File Size (MB)'  # Sets column header
 
 @admin.register(Gallery)
 class GalleryAdmin(admin.ModelAdmin):
