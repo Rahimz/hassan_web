c901343b52c52b3891fc8238c6ecea863e5401d3
rahim <rahim.aghareb@gmail.com>
2025-05-18 17:44:17 +0330

    add logger

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -1,4 +1,5 @@
 from .base import *
+import logging.config
 
 SECRET_KEY = pro_secret_key
 
@@ -23,3 +24,41 @@ CSRF_TRUSTED_ORIGINS = [
     'https://agharebparast.ir',
     'https://www.agharebparast.ir',
 ]
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'rotating_file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_errors.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 10,  # Keep 10 backup files
+            'formatter': 'verbose',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['rotating_file', 'console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
